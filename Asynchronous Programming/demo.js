109fc525729c00f76c63eafe04954f5204c237ae
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-02-21 21:20:54 +0200

    Update demo.js

--- Asynchronous Programming/demo.js
+++ Asynchronous Programming/demo.js
@@ -1,5 +1,19 @@
-console.log("Hello.");
-setTimeout(function() {
-console.log("Goodbye!");
-}, 2000);
-console.log("Hello again!");
+// console.log("Hello.");
+// setTimeout(function() {
+// console.log("Goodbye!");
+// }, 2000);
+// console.log("Hello again!");
+
+console.log('Before promise');
+
+
+new Promise(function(resolve, reject) {
+    setTimeout(function() {
+    resolve('done');
+    }, 500);
+    })
+    .then(function(res) {
+    console.log('Then returned: ' + res);
+    });
+
+    console.log('After promise');    
