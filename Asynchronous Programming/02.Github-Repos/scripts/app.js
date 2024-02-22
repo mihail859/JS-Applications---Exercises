f0caecf46c96c43a17ab7000868a758ae8f18e38
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-02-22 16:37:08 +0200

    Update app.js

--- Asynchronous Programming/02.Github-Repos/scripts/app.js
+++ Asynchronous Programming/02.Github-Repos/scripts/app.js
@@ -1,3 +1,22 @@
 function loadRepos() {
-	console.log("TODO...");
+	const username = document.getElementById('username').value;
+	const url = `: https://api.github.com/users/${username}/repos`;
+	const output = document.getElementById('repos');
+
+	fetch(url)
+	.then(response => {
+		if(response.ok == false){
+			console.log('error')
+			output.innerHTML = `<p>${response.status}: ${response.statusText}</p>`
+		}else{
+			return response.json()
+		}	
+	}).then(data => {
+		output.replaceChildren();
+		for (let entry of data){
+			const li = document.createElement('li')
+			li.innerHTML = `<a href="${entry.html_url}">${entry.full_name}</a>`;
+			output.appendChild(li);
+		}
+	})
 }
