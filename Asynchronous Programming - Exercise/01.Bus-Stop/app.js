169bc91fbfb050b95aea84eac713e4ce409d1a5a
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-02-24 09:34:45 +0200

    update

--- Asynchronous Programming - Exercise/01.Bus-Stop/app.js
+++ Asynchronous Programming - Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,20 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const busesUlList = document.getElementById('buses')
+
+    const stopId = document.getElementById('stopId').value;
+
+    const response = await fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`);
+
+    const data = await response.json();
+
+    
+    document.getElementById('stopName').textContent = data.name;
+
+    for (let [busId, time] of Object.entries(data.buses)){
+        const liElement = document.createElement('li');
+        liElement.textContent = `Bus ${busId} arrives in ${time} minutes`;
+
+        busesUlList.appendChild(liElement)
+
+    }
 }
