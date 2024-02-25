f4230b56fe5a0fed70abb61a9a8e9726d2dcee11
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-02-25 21:42:24 +0200

    Update demo.js

--- Remote Data and Authentication Lab/demo.js
+++ Remote Data and Authentication Lab/demo.js
@@ -10,6 +10,20 @@ async function addData(e){
     let yearStart = document.getElementById('yearStart').value; 
     let yearEnd = document.getElementById('yearEnd').value; 
 
+    let url = 'http://localhost:3030/jsonstore/landmarks';
+    let settings = {
+        method: 'Post',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name, area, yearStart, yearEnd})
+    };
     
+
+    let response = await fetch(url, settings);
+    let result = await response.json();
+
+    console.log(result)
+
 
 }
