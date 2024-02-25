8807a3f3fd3076d764db763bb44b4cd3a87807fc
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-02-25 21:17:35 +0200

    update

--- Remote Data and Authentication Lab/demo.js
+++ Remote Data and Authentication Lab/demo.js
@@ -1,4 +1,15 @@
-function loadData(e){
+let myBtn = document.getElementById('submitBtn')
+myBtn.addEventListener('click', addData);
+
+async function addData(e){
     e.preventDefault();
+    console.log('event prevented');
+
+    let name = document.getElementById('name').value; 
+    let area = document.getElementById('area').value; 
+    let yearStart = document.getElementById('yearStart').value; 
+    let yearEnd = document.getElementById('yearEnd').value; 
+
     
+
 }
