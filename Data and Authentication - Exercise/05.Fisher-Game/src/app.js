68ca2740aec5b27425cf6da8e092eda2507fad4b
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-02-27 19:52:53 +0200

    Update app.js

--- Data and Authentication - Exercise/05.Fisher-Game/src/app.js
+++ Data and Authentication - Exercise/05.Fisher-Game/src/app.js
@@ -1,1 +1,14 @@
 console.log('TODO:// Implement Home functionality');
+
+function app(){
+    const accessToken = sessionStorage.getItem('accessToken');
+    const loggedUserEmail = sessionStorage.getItem('loggedInUser');
+
+    if (loggedUserEmail) {
+        document.querySelector('span').textContent = loggedUserEmail;
+    }else{
+        document.querySelector('span').textContent = 'quest';
+    }
+}
+
+app();
