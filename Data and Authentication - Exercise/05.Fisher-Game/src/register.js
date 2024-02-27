81ecaa805e4a7d2c0f8014388dacb748e9e26b18
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-02-27 17:10:34 +0200

    Update register.js

--- Data and Authentication - Exercise/05.Fisher-Game/src/register.js
+++ Data and Authentication - Exercise/05.Fisher-Game/src/register.js
@@ -2,16 +2,39 @@ console.log('TODO:// Implement Register functionality');
 
 function register(){
     const registerBtn = document.querySelector('button');
+    let notificationParagraph = document.querySelector('.notification')
+    console.log(notificationParagraph);
 
-    registerBtn.addEventListener('click', onRegister)
-}
+    let  form = document.querySelector('form');
+    console.log(form);
+
+    registerBtn.addEventListener('click', onRegister);
 
 
 
-
-async function onRegister(e){
-    e.preventDefault();
+    async function onRegister(e){
+        e.preventDefault();
     
+        const formData = new FormData(form);
+        let email = formData.get('email');
+        let password = formData.get('password');
+        let rePass = formData.get('rePass');
+
+        console.log(email, password, rePass);
+
+        if (!email){
+            notificationParagraph.textContent = 'Please enter your email';
+        }else if (!password){
+            notificationParagraph.textContent = 'Please enter your password';
+        }else if (rePass !== password){
+            notificationParagraph.textContent = 'Both passwords must match!';
+        }
+
+        if (email && password && rePass){
+
+        }
+    
+    }
 }
 
 
