fa1d44ad0da80e643b1334d50d8cb303eeb91459
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-02-27 16:51:18 +0200

    Update register.js

--- Data and Authentication - Exercise/05.Fisher-Game/src/register.js
+++ Data and Authentication - Exercise/05.Fisher-Game/src/register.js
@@ -1,1 +1,20 @@
 console.log('TODO:// Implement Register functionality');
+
+function register(){
+    const registerBtn = document.querySelector('button');
+
+    registerBtn.addEventListener('click', onRegister)
+}
+
+
+
+
+async function onRegister(e){
+    e.preventDefault();
+    
+}
+
+
+
+
+register();
