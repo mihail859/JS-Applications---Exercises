524095f92ac1af944cf0dcc71a1df82b4f94a499
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-02-28 18:25:42 +0200

    Update app.js

--- Data and Authentication - Exercise/01.Messenger/app.js
+++ Data and Authentication - Exercise/01.Messenger/app.js
@@ -1,5 +1,47 @@
+const url = 'http://localhost:3030/jsonstore/messenger';
+const textArea = document.getElementById('messages');
+
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('submit').addEventListener('click', addMessage);
+    document.getElementById('refresh').addEventListener('click', refreshMessages);
 }
 
 attachEvents();
+async function addMessage(){
+    alert('the message has been added');
+    let author = document.querySelector('input[name="author"]').value;
+    let content = document.querySelector('input[name="content"]').value;
+
+    if (author && content){
+        let dataToBeAdd = {
+            author,
+            content
+        }
+
+        let response = await fetch(url, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(dataToBeAdd)
+        });
+
+        let result = await response.json();
+
+        console.log(result);
+
+    }
+}
+
+async function refreshMessages(){
+
+    let toLoadResponse = await fetch(url);
+    let dataToShow = await toLoadResponse.json();
+
+
+
+    Object.values(dataToShow).forEach(el => {
+        console.log(el);
+        textArea.textContent += `${el.author}: ${el.content}\n`
+    })
+
+}
+
