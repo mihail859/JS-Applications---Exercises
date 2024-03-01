1661fce848b6b90a637b7e4dad89223a8e725c3d
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-03-01 22:25:46 +0200

    update

--- Data and Authentication - Exercise/04.Book-library/app.js
+++ Data and Authentication - Exercise/04.Book-library/app.js
@@ -1,7 +1,46 @@
-const loadAllBooks = document.getElementById('loadBooks');
+const url = 'http://localhost:3030/jsonstore/collections/books';
 
+const loadAllBooks = document.getElementById('loadBooks');
 loadAllBooks.addEventListener('click', loadBooksFunction);
 
+const tbody = document.querySelector('tbody');
+console.log(tbody);
+
+
+const formElement = document.querySelector('form');
+formElement.addEventListener('submit', addBook);
+
 async function loadBooksFunction(){
-    console.log('it works')
+    let response = await fetch(url);
+    let data = await response.json();
+
+    Object.entries(data).forEach(([k, e]) => {
+
+        console.log(k, e);
+        let trElement = document.createElement('tr');
+        let td1 = document.createElement('td');
+        td1.textContent = e.author;
+
+        let td2 = document.createElement('td');
+        td2.textContent = e.title;
+
+        let td3 = document.createElement('td');
+
+        let btnEdit = document.createElement('button');
+        btnEdit.textContent = 'Edit';
+        btnEdit.classList.add(k);
+
+        let btnDelete = document.createElement('button');
+        btnDelete.textContent = 'Delete';
+        btnDelete.classList.add(k);
+
+        td3.appendChild(btnEdit);
+        td3.appendChild(btnDelete);
+
+        trElement.appendChild(td2);
+        trElement.appendChild(td1);
+        trElement.appendChild(td3);
+
+        tbody.appendChild(trElement)
+    })
 }
