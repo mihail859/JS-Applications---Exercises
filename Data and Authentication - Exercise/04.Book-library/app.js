d04ae1130eb460d0eaa9e13005d89cbcf8377f0b
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-03-01 22:11:26 +0200

    Update app.js

---
+++ Data and Authentication - Exercise/04.Book-library/app.js
@@ -0,0 +1,7 @@
+const loadAllBooks = document.getElementById('loadBooks');
+
+loadAllBooks.addEventListener('click', loadBooksFunction);
+
+async function loadBooksFunction(){
+    console.log('it works')
+}
