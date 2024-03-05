cded72f87e88403b8e1d267f877fc0fd32a1676f
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-03-05 16:00:47 +0200

    update

--- Single Page Applications - Exercise/02.Movies/src/app.js
+++ Single Page Applications - Exercise/02.Movies/src/app.js
@@ -1,8 +1,33 @@
+import { homeFunction } from './home.js'
+import { logoutFunction } from './logout.js'
+import { loginFunction } from './login.js'
+import { registerFunction } from './register.js'
+import { hideElements } from './hideElements.js'
+
+const hrefLinks = {
+    '/': homeFunction,
+    '/logout':logoutFunction ,
+    '/login': loginFunction,
+    '/register': registerFunction
+}
 
 
-// const hrefLinks = {
-//     '/': ,
-//     '/logout': ,
-//     'login': ,
-//     'register': 
-// }
+let navBar = document.querySelectorAll('.navbar a');
+navBar.forEach(link =>{
+    link.addEventListener('click', (e)=>{
+        e.preventDefault();
+        console.log(e.target.tagName);
+
+        const url = new URL(link.href);
+        const path = url.pathname
+        if (hrefLinks.hasOwnProperty(path)){
+            let callFunc = hrefLinks[path];
+            callFunc();
+        }
+    })
+});
+
+let homeView = document.getElementById('home-page');
+
+// hideElements(homeView);
+
--- Single Page Applications - Exercise/02.Movies/src/logout.js
+++ Single Page Applications - Exercise/02.Movies/src/logout.js
@@ -1,3 +1,3 @@
 export function logoutFunction(){
-    
+    console.log('logout function');
 }
