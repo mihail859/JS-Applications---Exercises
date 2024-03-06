f27058ecca9bfc0f5f90f9a0f5b1d1aab29ff85b
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-03-06 23:53:28 +0200

    Update app.js

--- Client Side Rendering - Lab/01.Contacts/app.js
+++ Client Side Rendering - Lab/01.Contacts/app.js
@@ -2,6 +2,36 @@ import {html, render } from '../01.Contacts/node_modules/lit-html/lit-html.js';
 import { contacts } from './contacts.js';
 
 
-let h2 = () => html`<h2>Hello world</h2>`;
-render(h2(), document.body)
+
+let contactsStructure = (elem) => html`
+        <div class="contact card">
+            <div>
+                <i class="far fa-user-circle gravatar"></i>
+            </div>
+            <div class="info">
+                <h2>Name: ${elem.name}</h2>
+                <button class="detailsBtn" @click=${() => showDetails(elem)}>Details</button>
+                <div class="details" id="${elem.id}">
+                    <p>Phone number: ${elem.phoneNumber}</p>
+                    <p>Email: ${elem.email}</p>
+                </div>
+            </div>
+        </div>`;
+
+
+start();
+
+async function start(){
+    let contactsField = document.getElementById('contacts');
+    render(contacts.map(contactsStructure), contactsField);
+}
+
+function showDetails(contact) {
+    const detailsElement = document.getElementById(contact.id);
+
+    detailsElement.style.display = detailsElement.style.display ===  'none' ? 'block' : 'none';
+}
+
+
+
 
