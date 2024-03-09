a448c2c82f1ff77cf312af04d71dd8b66cae1ae7
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-03-09 09:16:51 +0200

    solve

--- Client Side Rendering - Exercise/03.Search-in-List/search.js
+++ Client Side Rendering - Exercise/03.Search-in-List/search.js
@@ -1,3 +1,36 @@
-function search() {
-   // TODO
+import { towns } from "./towns.js";
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+
+const btnSearch = document.querySelector('button')
+btnSearch.addEventListener('click', search)
+
+function loadListItems(){
+   let template = html`
+      <ul>
+         ${towns.map((t) => html`<li id=${t}>${t}</li>`)}
+      </ul>
+   `;
+
+   render(template, document.getElementById('towns'))
 }
+
+loadListItems();
+
+function search(e) {
+   e.preventDefault();
+
+   console.log('works.... ');
+   let searchedWord = document.getElementById('searchText').value;
+
+   let matches = 0;
+   towns.forEach((t) => {
+      if (t.includes(searchedWord)){
+         document.getElementById(t).setAttribute('class', 'active');
+         console.log('Towns', t);
+         matches++;
+      }
+   })
+   document.getElementById('result').textContent = `${matches} matches found`
+
+}
+
