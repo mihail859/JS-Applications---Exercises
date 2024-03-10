9679584b3899521b3a9882571ea151d1ba48004c
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-03-10 20:04:34 +0200

    Update app.js

--- Client Side Rendering - Exercise/06.Book-Library/app.js
+++ Client Side Rendering - Exercise/06.Book-Library/app.js
@@ -1,1 +1,10 @@
-console.log('My requests...')
+import { html, render } from './node_modules/lit-html/lit-html.js';
+import { mainTemplate } from './templates.js';
+
+render(mainTemplate, document.querySelector('body'))
+
+
+
+
+
+
