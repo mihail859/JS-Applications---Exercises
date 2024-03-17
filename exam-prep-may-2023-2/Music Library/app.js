af22093d9acba594dcdc9c667310c869c726a09b
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-03-17 12:15:29 +0200

    update

--- exam-prep-may-2023-2/Music Library/app.js
+++ exam-prep-may-2023-2/Music Library/app.js
@@ -6,6 +6,7 @@ import { logInView } from "./views/login.js"
 import { logOutView } from "./views/logout.js"
 import { registerView } from "./views/register.js"
 import { detailsView } from "./views/details.js"
+import { editView } from "./views/edit.js"
 console.log("Works...")
 
 page(updateNavBar)
@@ -15,5 +16,6 @@ page("/login", logInView)
 page("/logout", logOutView)
 page("/register", registerView)
 page("/details/:id", detailsView);
+page("/edit/:id", editView)
 
 page.start();
---
+++ exam-prep-may-2023-2/Music Library/views/edit.js
@@ -0,0 +1,5 @@
+export async  function editView(ctx, next){
+    console.log("Edit view")
+    let id = ctx.params.id;
+    console.log(id);
+}
