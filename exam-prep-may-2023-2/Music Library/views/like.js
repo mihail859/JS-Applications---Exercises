15d29ce8d96d04b4a8a12e5c9629eb4b9eb8ad74
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-03-17 15:21:31 +0200

    Update like.js

--- exam-prep-may-2023-2/Music Library/views/like.js
+++ exam-prep-may-2023-2/Music Library/views/like.js
@@ -1,3 +1,34 @@
-export async function like(){
+export async function like(ctx){
     console.log("From like")
+    
+
+    try {
+        let albumId = ctx.params.id;
+        console.log(albumId)
+
+        let response = await fetch()
+
+
+    } catch (error) {
+        
+    }
+}
+
+async function publishLike(albumId){
+    try {
+        let response = await fetch("http://localhost:3030/data/likes", {
+            method: "POST",
+            headers: {"Content-Type" : "application/json"},
+            body: JSON.stringify({ albumId })
+        })
+        if (!response.ok){
+            throw new Error(response.statusText)
+        }
+        let data = await response.json();
+        console.log(data);
+
+
+    } catch (error) {
+        alert(error.message)
+    }
 }
